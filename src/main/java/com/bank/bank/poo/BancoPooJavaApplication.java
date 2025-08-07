3f34889492b5f6d91bf79dfaafb449d5967513d1
Cleo Silva <cleo_silva1906@hotmail.com>
2025-08-07 17:20:08 -0300

    feat: Starting project

---
+++ src/main/java/com/bank/bank/poo/BancoPooJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bank.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPooJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoPooJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/bank/poo/BancoPooJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bank.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoPooJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
